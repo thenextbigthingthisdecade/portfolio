abe507b806c4268f5b6e29c619e3c40781f0a414
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-04-01 19:10:10 +0100

    removed uneccesary files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
-      <main className="text-center">
+      <main>
         <div></div>
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {},
+    },
   },
   plugins: [],
 };
