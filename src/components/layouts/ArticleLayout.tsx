19092279ad67067f4e27c7e97ebacb4b63bb1123
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-04-15 21:19:53 +0100

    added date to article props

--- src/components/layouts/ArticleLayout.tsx
+++ src/components/layouts/ArticleLayout.tsx
@@ -17,12 +17,13 @@ const fraunces = Fraunces({
 export default function ArticleLayout({
   title,
   children,
-}: PropsWithChildren & { title: string }) {
+  date,
+}: PropsWithChildren & { title: string; date: string }) {
   return (
     <div className="font-reading mt-12 md:mt-20 px-4 sm:px-6 lg:px-8 mx-auto max-w-3xl text-article-primary leading-[1.7]">
       <div className="mb-10">
         <h1 className="font-sans text-base font-medium mb-1">{title}</h1>
-        <h3 className="text-article-note text-sm">14 April, 2023</h3>
+        <h3 className="text-article-note text-sm">{date}</h3>
       </div>
       {children}
     </div>
