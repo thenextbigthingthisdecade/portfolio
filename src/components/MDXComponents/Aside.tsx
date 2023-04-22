bb90e387dbda1053034ecfcc6372da6c722f071b
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-04-22 17:55:09 +0400

    removed .env file

--- src/components/MDXComponents/Aside.tsx
+++ src/components/MDXComponents/Aside.tsx
@@ -19,7 +19,7 @@ export function Aside({
         //   special rules and animations
         "[&_*]:pb-0 transition-all duration-200 ease-linear flex-col",
         //   base styling
-        "p-2 rounded-md mb-5 md:mb-7 bg-neutral-50/10",
+        "p-2 rounded-md mb-5 md:mb-7 bg-neutral-100/50",
         // hover
         "hover:bg-neutral-100"
       )}
