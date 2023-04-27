a4ccc0cdb5dceca013cb7d117eee8832fba45f2a
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-04-27 14:23:20 +0400

    fixed kbd ket

--- src/components/Kbd.tsx
+++ src/components/Kbd.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Kbd({ children }: PropsWithChildren) {
   return (
-    <span className="inline-block shadow-inset-skeuo rounded px-1.5 font-mono tracking-tighter text-base font-light bg-neutral-50/90 text-neutral-900">
+    <span className="inline-block shadow-inset-skeuo rounded px-1.5 font-mono tracking-tighter text-base bg-neutral-50/90 text-neutral-600">
       {children}
     </span>
   );
