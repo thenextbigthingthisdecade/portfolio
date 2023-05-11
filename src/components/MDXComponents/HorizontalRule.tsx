f44966af95ec848b0552836b95c9ccc28320f01a
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-05-11 18:34:20 +0100

    sinish

--- src/components/MDXComponents/HorizontalRule.tsx
+++ src/components/MDXComponents/HorizontalRule.tsx
@@ -1,5 +1,5 @@
 export function HorizontalRule() {
   return (
-    <hr className="mb-5 -mt-4 md:-mt-6 md:mb-7 border-article-note/20 w-full" />
+    <hr className="mb-5 mt-0 md:-mt-1 md:mb-7 border-article-note/20 w-full" />
   );
 }
